5d64d826277001d6d9cf2ad800be83e18ea1a282
Yosteven <yosteven.yosteven@adira.co.id>
2023-02-19 22:53:30 +0700

    eslint airbnbc react-recommendation

---
+++ test.js
@@ -0,0 +1,5 @@
+const a = 'Sed ex arcu, aliquet non luctus in, pharetra eget purus. Phasellus porta mauris sem, at egestas quam vehicula a. Sed eleifend risus sit amet orci maximus, et tincidunt diam tincidunt. Integer neque dui, egestas nec nisl sed, volutpat varius mauris. Phasellus consectetur congue maximus. Vivamus consequat ante ac nisi elementum, vel interdum nulla pulvinar. Curabitur ac nulla nibh. Quisque placerat tempus ipsum vitae tempus. Integer posuere pulvinar sem et posuere. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed erat commodo justo accumsan dapibus. Vestibulum malesuada, tortor sit amet eleifend tempor, lectus est maximus enim, nec semper erat velit sit amet tortor. Sed blandit, lectus sit amet bibendum ultricies, orci arcu pretium est, sit amet auctor urna enim quis risus. Pellentesque varius ipsum nisi, ac dictum orci venenatis nec.';
+
+let b;
+
+running = (b) => b;
